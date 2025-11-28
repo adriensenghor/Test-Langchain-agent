7b467b7349b0574cb1d0e281e65552d9d86c9f66
adriensenghor <adriensenghor@outlook.com>
2025-11-28 13:43:49 +0100

    petit fix des requirements et du model

--- app/model.py
+++ app/model.py
@@ -3,6 +3,6 @@ from langchain_anthropic import ChatAnthropic
 def get_model(temperature=0):
     # Claude 3.5 Sonnet est le meilleur ratio coût/intelligence
     return ChatAnthropic(
-        model="claude-3-5-sonnet-latest",
+        "claude-sonnet-4-5-20250929",
         temperature=temperature
     )
